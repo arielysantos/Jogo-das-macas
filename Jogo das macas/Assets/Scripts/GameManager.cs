dd1dae0e43fe3e19a734b6dca58c3ee1201c05fd
zaikodan <zaikodan@gmail.com>
2024-09-17 08:56:07 -0300

    AddScore

--- Jogo das macas/Assets/Scripts/GameManager.cs
+++ Jogo das macas/Assets/Scripts/GameManager.cs
@@ -6,5 +6,27 @@ using Photon.Pun;
 
 public class GameManager : MonoBehaviourPun
 {
-    
+    Vector2 screenBounds;
+    int score, playerInGame;
+    Text scoreText;
+
+    public static GameManager instance;
+
+    public Vector2 ScreenBounds { get => screenBounds; }
+
+    private void Awake()
+    {
+        instance = this;
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+        screenBounds += new Vector2(-1, 1);
+
+        photonView.RPC("AddPlayer", RpcTarget.AllBuffered);
+    }
+
+    [PunRPC]
+    void AddScore(int value)
+    {
+        score += value;
+        scoreText.text = score.ToString();
+    }
 }
