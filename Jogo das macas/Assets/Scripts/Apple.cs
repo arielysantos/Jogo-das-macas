ad0e386c1205089ea23d03efd2382d2ccdff3b05
zaikodan <zaikodan@gmail.com>
2024-09-17 09:34:45 -0300

    Update Apple.cs

--- Jogo das macas/Assets/Scripts/Apple.cs
+++ Jogo das macas/Assets/Scripts/Apple.cs
@@ -5,5 +5,30 @@ using Photon.Pun;
 
 public class Apple : MonoBehaviourPun
 {
-    
+    const int speed = 5;
+    [SerializeField] int score;
+    Rigidbody2D rigidbody2D;
+
+    public int Score { get => score; }
+
+    private void Awake()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        rigidbody2D.velocity = Vector2.down * speed;
+
+        if(transform.position.y < -GameManager.instance.ScreenBounds.y)
+        {
+            photonView.RPC("Destroy", RpcTarget.All);
+        }
+    }
+
+    [PunRPC]
+    void Destroy()
+    {
+        Destroy(gameObject);
+    }
 }
