290d2d0c5accd84031a3934c7a47fe2863415e27
zaikodan <zaikodan@gmail.com>
2024-09-17 09:02:37 -0300

    AddPlayer e CreatePlayer

--- Jogo das macas/Assets/Scripts/GameManager.cs
+++ Jogo das macas/Assets/Scripts/GameManager.cs
@@ -7,7 +7,7 @@ using Photon.Pun;
 public class GameManager : MonoBehaviourPun
 {
     Vector2 screenBounds;
-    int score, playerInGame;
+    int score, playersInGame;
     Text scoreText;
 
     public static GameManager instance;
@@ -28,5 +28,21 @@ public class GameManager : MonoBehaviourPun
     {
         score += value;
         scoreText.text = score.ToString();
+    }
+
+    [PunRPC]
+    void AddPlayer()
+    {
+        playersInGame++;
+
+        if(playersInGame == PhotonNetwork.PlayerList.Length)
+        {
+            CreatePlayer();
+        }
+    }
+
+    void CreatePlayer()
+    {
+        NetworkManager.instance.Instantiate("Prefabs/Player", new Vector2(0,-4), Quaternion.identity);
     }
 }
